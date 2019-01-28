40478a0bf1d1ab738bed4098434d3273f35cbe52
David Sarginson <david.sarginson@opencastsoftware.com>
2019-01-28 14:01:51 +0000

    Initial Commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject algorithms_1 "0.1.0-SNAPSHOT"
+  :description "Code relating to the Coursera Algorithms part 1 course"
+  :url "http://example.com/FIXME"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :main ^:skip-aot algorithms-1.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
